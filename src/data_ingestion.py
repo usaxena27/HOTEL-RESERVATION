75a2e8ff01a93106a34f232c4fbcf6406c070b52
Utkarsh Saxena <saxena2701@gmail.com>
2025-12-13 11:24:48 +0530

    commit

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -33,7 +33,7 @@ class DataIngestion:
         
     def split_data(self):
         try:
-            logger.info("Starting the splitting  of data into train and test sets")
+            logger.info("Starting the splitting of data into train and test sets")
             data = pd.read_csv(RAW_FILE_PATH)
             train_data, test_data = train_test_split(data, test_size=1-self.train_test_ratio, random_state=42)
             train_data.to_csv(TRAIN_FILE_PATH, index=False)
